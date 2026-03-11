0e814baaf699cb2a2d7ed52c032f77339bb7690c
wjswpdbs7-code <wjswpdbs7@gmail.com>
2026-03-12 06:43:25 +0900

    03 12

--- 03.11/dice.c
+++ 03.11/dice.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
+int main(void)//void는 함수가 아무것도 반환하지 않는다는 것을 나타냅니다. VOID대신 다른
+//걸 써야하는 경우를 들자면 예를들어 int main(int argc, char *argv[]) 같은 경우는 명령행 인자를 받아들이는 main 함수의 형태입니다. 
+// 보통 어떤 파일을 만들때 쓰냐면
 {
     int dice;
     srand(time(NULL));
---
+++ 03.11/lan.c
@@ -0,0 +1,14 @@
+//int main(void), include<stdio.h>, return 0;, printf, scanf, 대충 이정도 쓸줄아는데 이걸로 연습할만한 문제하나 내
+//1. 1부터 10까지의 숫자를 출력하는 프로그램
+#include<stdio.h>
+
+int main(void)
+{
+        printf("be happy\n");
+        printf("12345678901234567890\n");
+        printf("my\tfriend\n");
+        printf("goot\bd\tchence\n");
+        printf("cow\rW\a\n");
+
+        return 0;
+}
